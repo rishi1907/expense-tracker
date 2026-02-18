39146c1e3791f54ca6fbbe87f9e12598f503ae66
rishi1907 <risikeshranjan074@gmail.com>
2026-02-18 19:27:35 +0530

    commit

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3000';
-// const API_BASE_URL = 'https://expense-tracker-liart-gamma-91.vercel.app';
+// const API_BASE_URL = 'http://localhost:3000';
+const API_BASE_URL = 'https://expense-tracker-liart-gamma-91.vercel.app';
 
 const api = axios.create({
     baseURL: API_BASE_URL,
