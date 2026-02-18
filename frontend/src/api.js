f551ffe8ed0c4b3446d59a39d1b12bdbf07c24fc
rishi1907 <risikeshranjan074@gmail.com>
2026-02-18 19:51:52 +0530

    deployed

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const API_BASE_URL = 'http://localhost:3000';
-const API_BASE_URL = 'https://expense-tracker-r4yl.vercel.app';
+const API_BASE_URL = 'https://expense-tracker-wn6h.vercel.app';
 
 const api = axios.create({
     baseURL: API_BASE_URL,
