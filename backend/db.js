c4b93390816368024eb13cb1e162529a71ab677b
rishi1907 <risikeshranjan074@gmail.com>
2026-02-20 15:06:38 +0530

    commit

--- backend/db.js
+++ backend/db.js
@@ -1,19 +1,13 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 
-let isConnected = false;
-
 const connectDB = async () => {
-    if (isConnected) {
-        return;
-    }
-
     try {
-        const db = await mongoose.connect(process.env.MONGO_URI);
-        isConnected = db.connections[0].readyState;
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('MongoDB Connected');
     } catch (err) {
         console.error('MongoDB connection error:', err.message);
-        throw err; // don't exit
+        process.exit(1);
     }
 };
 
